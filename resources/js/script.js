5c235547020636d25537d7df4d2ffa5d6fa7307c
Diego Alexander Londoño <dlondonom@gmail.com>
2016-10-31 15:31:49 -0500

    v1 para producción

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,14 +1,20 @@
+
 (function ($, window, document, undefined) {
     'use strict';
 
+    function marginTopSections(section, css) {
+        var top = ( $(window).height() - section.height() ) * 0.5;
+        section.css(css, top);
+    }
+
     function initReady() {
-
+        marginTopSections($('h1'), 'margin-top');
     }
 
-    function initLoad() {
-
+    function initResizeWindow() {
+        marginTopSections($('h1'), 'margin-top');
     }
 
-    $(document).on('ready', initReady);
-    $(window).on('load', initLoad);
+    $(document).ready(initReady);
+    $(window).on('resize', initResizeWindow);
 })(jQuery, window, document);
